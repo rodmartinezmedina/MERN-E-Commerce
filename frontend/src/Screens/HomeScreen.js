a3c169c50c1d930ce8783ef6141839d7b5048913
John Doe <rodmartinezmedina@gmail.com>
2021-01-09 14:57:38 +0100

    basic backend setup with products.json mock data on backend Frontend reading data with axios from the products in the backend

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+    console.log('UseEffect on Home Screen.. Products are: ', products)
+  }, [])
+
   return (
     <>
       <h3>Latest Products</h3>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
