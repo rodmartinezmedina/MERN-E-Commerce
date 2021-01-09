7e28bda38c15d42c75f658e52d90843798081504
John Doe <rodmartinezmedina@gmail.com>
2021-01-09 13:19:25 +0100

    links on navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Navbar, Nav, Container, NavLink } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container>
-          <Navbar.Brand href='/'>E-commerce Brand</Navbar.Brand>
+          <Link to='/'>
+            <Navbar.Brand>E-commerce Brand</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <Link to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </Link>
+              <Link to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
