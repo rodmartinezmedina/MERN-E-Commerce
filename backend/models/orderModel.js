70ebcaa017d1d745d8865a3d1f23571fb22ada96
John Doe <rodmartinezmedina@gmail.com>
2021-01-10 18:55:04 +0100

    added commands for seeding data and imported seed data to mongoDB

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const orderSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Type.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: 'User',
     },
