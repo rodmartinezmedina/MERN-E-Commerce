2aa6a121d21867ee61cdb09bd6bef1293bfdaea7
John Doe <rodmartinezmedina@gmail.com>
2021-01-14 01:02:27 +0100

    update homeScreenusing redux productRoutes file

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,31 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  // thiis dispatch is the equivalent to connect used With the mapStatetoProps
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-
-    fetchProducts()
-    console.log('UseEffect on Home Screen.. Products are: ', products)
-  }, [])
+    dispatch(listProducts())
+    // const fetchProducts = async () => {
+    //   const { data } = await axios.get('/api/products')
+    //   setProducts(data)
+    // }
+    // fetchProducts()
+    // console.log('UseEffect on Home Screen.. Products are: ', products)
+  }, [dispatch])
 
   return (
     <>
       <h3>Latest Products</h3>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h4>Loading...</h4>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
