a5becaf0c3fb4aa05ab110e1bd1b7986a5d28f2e
John Doe <rodmartinezmedina@gmail.com>
2021-01-19 00:52:04 +0100

    user controller, match password method in user model, changes in user route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+// Auth user & get token
+// POST / api/users/login
+// Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  //   res.send({ email, password })
+  const user = await User.findOne({ email: email })
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error(`Invalid Email or password`)
+  }
+})
+
+export { authUser }
